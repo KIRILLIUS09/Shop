7dc759b0e5a87060c64a03479752ab97cb347534
KIRILLIUS09 <132893081+KIRILLIUS09@users.noreply.github.com>
2025-06-13 18:20:03 +0300

    Add files via upload

---
+++ WebApi/Filter/ProductFilter.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi.Filters
+{
+    public class ProductFilter
+    {
+        public string? Color { get; set; }
+        public string? Size { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+    }
+}
