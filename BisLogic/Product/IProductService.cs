04084734fb73f60b2030181b99b66fd38b2dcf2c
KIRILLIUS09 <132893081+KIRILLIUS09@users.noreply.github.com>
2025-06-13 17:57:20 +0300

    Add files via upload

---
+++ BisLogic/Product/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using Core.Models;
+
+namespace BisLogic.Services
+{
+    public interface IProductService
+    {
+        Task<Product> CreateProductAsync(Product product);
+        Task<Product> GetProductByIdAsync(int id);
+        Task<List<Product>> GetProductsAsync();
+        Task UpdateProductAsync(Product product);
+        Task DeleteProductAsync(int id);
+    }
+}
