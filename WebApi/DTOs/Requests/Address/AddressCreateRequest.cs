5ede82aacaf7651bb0c9d3a715b40a5e62f48063
KIRILLIUS09 <132893081+KIRILLIUS09@users.noreply.github.com>
2025-06-13 18:25:31 +0300

    Add files via upload

---
+++ WebApi/DTOs/Requests/Address/AddressCreateRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.DTOs.Requests.Address
+{
+    public record AddressCreateRequest(
+        [Required] string Region,
+        [Required] string City,
+        [Required] string Street,
+        [Required] string Building,
+        string? Apartment,
+        [Required] int UserId
+    );
+}
