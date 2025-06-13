b6d4d5a9d27d078be2ef6c2baf6d5da6d91921e5
KIRILLIUS09 <132893081+KIRILLIUS09@users.noreply.github.com>
2025-06-13 18:10:25 +0300

    Add files via upload

---
+++ Core/Interfaces/IAppDbContext.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Interfaces
+{
+    public interface IAppDbContext
+    {
+        DbSet<Address> Addresses { get; }
+        DbSet<CartItem> CartItems { get; }
+        DbSet<Order> Orders { get; }
+        DbSet<Product> Products { get; }
+        DbSet<User> Users { get; }
+
+        void AddEntity<TEntity>(TEntity entity) where TEntity : class;
+        void RemoveEntity<TEntity>(TEntity entity) where TEntity : class;
+        void UpdateEntity<TEntity>(TEntity entity) where TEntity : class;
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+        int SaveChanges();
+
+        // Универсальный интерфейс транзакции
+        Task<IDbContextTransaction> BeginTransactionAsync(CancellationToken cancellationToken = default);
+    }
+
+    public interface IDbContextTransaction : IDisposable
+    {
+        void Commit();
+        void Rollback();
+    }
+}
