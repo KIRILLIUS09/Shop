7ea5b0918e3dbd9fe8906d7a9926a0f240dc9714
KIRILLIUS09 <132893081+KIRILLIUS09@users.noreply.github.com>
2025-06-13 18:25:55 +0300

    Add files via upload

---
+++ WebApi/DTOs/Requests/Cart/CartItemAddRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.DTOs.Requests.Cart
+{
+    public record CartItemAddRequest(
+        [Required(ErrorMessage = "ProductId обязателен")]
+        int ProductId,
+
+        [Range(1, 100, ErrorMessage = "Количество должно быть от 1 до 100")]
+        int Quantity = 1 
+    );
+}
---
+++ WebApi/DTOs/Requests/Cart/UpdateCartItemRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.DTOs.Requests.Cart
+{
+    public record UpdateCartItemRequest(
+        [Range(1, 100, ErrorMessage = "Количество должно быть от 1 до 100")]
+        int NewQuantity
+    );
+}
