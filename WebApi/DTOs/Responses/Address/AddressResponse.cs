a5f914f825965c4e42853521d697de1192ad4615
KIRILLIUS09 <132893081+KIRILLIUS09@users.noreply.github.com>
2025-06-13 18:31:21 +0300

    Add files via upload

---
+++ WebApi/DTOs/Responses/Address/AddressResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.DTOs.Responses.Address
+{
+    public record AddressResponse(
+    int Id,
+    string FullAddress,  
+    string Region,
+    string City,
+    string Street,
+    string Building,
+    string? Apartment,
+    int? UserId
+);
+}
---
+++ WebApi/DTOs/Responses/Address/AddressShortResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.DTOs.Responses.Address
+{
+    public record AddressShortResponse(
+     int Id,
+     string ShortAddress,
+     int? UserId
+ );
+}
