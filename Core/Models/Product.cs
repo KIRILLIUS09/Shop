e220d2cfbb13a6ebbe1c0a9a3f1d0e7adcb641f8
KIRILLIUS09 <132893081+KIRILLIUS09@users.noreply.github.com>
2025-06-15 11:01:37 +0300

    Update Product.cs

--- Core/Models/Product.cs
+++ Core/Models/Product.cs
@@ -1,21 +1,31 @@
-﻿namespace Core.Models
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Models
 {
     public class Product
     {
-       
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ProductId { get; set; }
 
-       
+        [Required]
+        [MaxLength(100)]
         public required string Name { get; set; }
 
+        [Required]
         public decimal Price { get; set; }
 
-       
+        [Required]
+        [MaxLength(200)]
         public required string Description { get; set; }
 
-       
+        [Required]
+        [MaxLength(50)]
         public required string Color { get; set; }
 
+        [Required]
+        [MaxLength(20)]
         public required string Size { get; set; }
 
         public List<CartItem> CartItems { get; set; } = [];
