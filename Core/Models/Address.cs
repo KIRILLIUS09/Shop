0e692dd0f44d181d3f7fefac769b25e41d6ea0fa
KIRILLIUS09 <132893081+KIRILLIUS09@users.noreply.github.com>
2025-06-15 10:47:20 +0300

    Update Address.cs

--- Core/Models/Address.cs
+++ Core/Models/Address.cs
@@ -1,29 +1,36 @@
-﻿namespace Core.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Core.Models
 {
     public class Address
     {
-        
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int AddressId { get; set; }
 
-        
+        [Required]
+        [MaxLength(50)] 
         public string Region { get; set; } = string.Empty;
 
-       
+        [Required]
+        [MaxLength(85)] 
         public string City { get; set; } = string.Empty;
 
-        
+        [Required]
+        [MaxLength(120)] 
         public string Street { get; set; } = string.Empty;
 
-       
+        [Required]
+        [MaxLength(15)] 
         public string Building { get; set; } = string.Empty;
 
+        [MaxLength(10)] 
         public string? Apartment { get; set; }
 
-        
+        [ForeignKey(nameof(User))]
         public int? UserId { get; set; }
 
         public User? User { get; set; }
-
-
     }
 }
