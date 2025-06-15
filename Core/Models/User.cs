82b0c3e24dd434f0a55869470cf0c16f31cc6498
KIRILLIUS09 <132893081+KIRILLIUS09@users.noreply.github.com>
2025-06-15 11:02:03 +0300

    Update User.cs

--- Core/Models/User.cs
+++ Core/Models/User.cs
@@ -1,30 +1,46 @@
-﻿namespace Core.Models
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Models
 {
     public class User
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
 
-        
+        [Required]
+        [MaxLength(255)]
+        [EmailAddress]
         public required string Email { get; set; }
 
+        [Required]
+        [MaxLength(20)]
+        [Phone]
         public required string PhoneNumber { get; set; }
 
-       
+        [Required]
+        [MaxLength(50)]
         public required string LastName { get; set; }
 
-        
+        [Required]
+        [MaxLength(50)]
         public required string FirstName { get; set; }
 
-       
+        [MaxLength(50)]
         public required string MiddleName { get; set; }
 
-       
+        [Required]
+        [MaxLength(30)]
         public required string Username { get; set; }
 
-       
+        [Required]
+        [MaxLength(255)]
         public required string PasswordHash { get; set; }
-        public string Role { get; set; } = "User";
 
+        [Required]
+        [MaxLength(20)]
+        public string Role { get; set; } = "User";
 
         public List<Address> Addresses { get; set; } = [];
         public List<Order> Orders { get; set; } = [];
