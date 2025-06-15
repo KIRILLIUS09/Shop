df90a92140dc79bfc76806f9c8f0430e69e1f3ac
KIRILLIUS09 <132893081+KIRILLIUS09@users.noreply.github.com>
2025-06-15 11:00:42 +0300

    Update CartItem.cs

--- Core/Models/CartItem.cs
+++ Core/Models/CartItem.cs
@@ -1,23 +1,28 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Core.Models
 {
     public class CartItem
     {
-        
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int CartItemId { get; set; }
 
-       
-        public int Quantity { get; set; } = 1;
+        [Required]
+        public int Quantity { get; set; } = 1; 
 
-       
+        [Required]
+        [ForeignKey(nameof(Product))]
         public int ProductId { get; set; }
-        
-        public int UserId { get; set; } 
 
+        [Required]
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
 
         public Product Product { get; set; } = null!;
 
+        [ForeignKey(nameof(Order))]
         public int? OrderId { get; set; }
 
         public Order? Order { get; set; }
